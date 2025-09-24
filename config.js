c07f908ab95310e72760da8ff5eb25d132082365
JJ <thehilabinc@gmail.com>
2025-09-24 20:44:15 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js 파일 내용을 이렇게 수정하세요
+window.SUPABASE_CONFIG = {
+    SUPABASE_URL: 'https://khytzjktxmqdaftjfmsq.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoeXR6amt0eG1xZGFmdGpmbXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxOTc1NTAsImV4cCI6MjA2OTc3MzU1MH0.GXMY5r3cgivmcGBRULqDB5faBUTYR9jf3MHxt4dtfBc'
+};
